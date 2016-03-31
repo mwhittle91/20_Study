3e8f51d49b8277d9aa6fd62220e6fc419433b6a7
Matthew Whittle <gy14mw@leeds.ac.uk>
2016-03-31 09:26:47 +0100

    download 20mph zones

--- download-osm-data.R
+++ download-osm-data.R
@@ -1,16 +1,22 @@
-# install.packages("osmplotr")
+#install.packages("osmplotr")
+#install.packages("tmap")
+#install.packages("rgdal")
+#install.packages("sp")
+#install.packages("mapview")
 library(osmplotr)
 library(tmap)
+library(sp)
 
 ?osmplotr
 b = bb("Leeds")
-b = as.vector(b)
 class(b)
+#b = as.vector(b)
+#class(b)
 h = extract_osm_objects(key = "highway", bbox =  b, extra_pairs = c("maxspeed", "20"))
 h60 = extract_osm_objects(key = "highway", bbox =  b, extra_pairs = c("maxspeed", "60"))
 summary(h)
 h = h$obj
 plot(h60$obj)
-plot(h, col = "red", add = T)
+plot(h, col = "red", add = TRUE)
 mapview::mapview(h)
 
